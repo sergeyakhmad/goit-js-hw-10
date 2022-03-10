cc0c014b48a317cfcdc1d6b638fe19ac02a09fa3
Sergey Akhmadaliev <Sergan86@ukr.net>
2022-03-10 13:21:11 +0200

    add export fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
@@ -7,15 +8,16 @@ const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
-searchCountry.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+searchCountry.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
 
-function onSearch(e) {
+function onSearchCountry(e) {
   e.preventDefault();
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
 
-  const searchField = e.target.value;
-  if (searchField.trim() === '') {
+  const searchField = e.target.value.trim();
+
+  if (searchField === '') {
     Notiflix.Notify.failure('Please, enter country name! The field is empty!');
     return;
   }
@@ -24,33 +26,16 @@ function onSearch(e) {
     .then(country => {
       if (country.length === 1) {
         countryMarkup(country);
-        Notiflix.Notify.success('See what we have found for you!');
         return;
       }
       if (country.length >= 2 && country.length <= 10) {
         severalCountriesMarkup(country);
-        Notiflix.Notify.success('We have found several options for you!');
         return;
       }
       Notiflix.Notify.warning('Too many matches found. Please enter a more specific name');
     })
     .catch(error => {
       Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
-}
-
-function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.log(error);
     });
 }
 
